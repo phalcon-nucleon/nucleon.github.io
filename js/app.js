70f0772485c7134b2c326b6b900d10c41369388a
Guillaume ALLEGRET <guillaume.allegret-prestataire@laposte.fr>
2018-04-04 18:09:26 +0200

    **

--- js/app.js
+++ js/app.js
@@ -17,36 +17,57 @@
 
   var Sidenav = {
     init: function () {
-      $('#slide-out')
-          .on('click', 'a.collapsible-header', function () {
-            $('#slide-out').find('li.active > a.collapsible-header').find('i').text('keyboard_arrow_right');
-            $(this).find('i').text('keyboard_arrow_down');
-          })
-          .on('click', 'a[data-template]', function () {
-            $('#slide-out').find('li.active > a[data-template]').parent().removeClass('active');
-            $(this).parent().addClass('active');
-          })
+      var $slide = $('#slide-out');
+      $slide.find('.collapsible').collapsible({
+        onOpenStart: function (elem) {
+          $(elem).find('i').text('keyboard_arrow_down');
+        },
+        onCloseStart: function (elem) {
+          $(elem).find('i').text('keyboard_arrow_right');
+        }
+      });
+
+      $slide.on('click', 'a[data-template]', function () {
+        Sidenav.select($(this))
+      })
+    },
+    select: function ($elem) {
+      $('#slide-out').find('li.active > a[data-template]').parent().removeClass('active');
+      $elem.parent().addClass('active');
+      M.Collapsible.getInstance($elem.parents('.collapsible').get(0)).open($elem.parents('.no-padding').index());
     }
   };
 
   var Template = {
     init: function () {
       $('body').on('click', '[data-template]', function () {
-        Template.show($(this).data('template'))
+        Template.show($(this).attr('href').substr(2))
       })
     },
     show: function (template) {
+      Sidenav.select($('a[href="#!' + template + '"]'));
+
       Loader.ajax({
-        url: 'templates/' + template + '.html'
+        url: 'templates/' + template
       }).done(function (data) {
-        $('[data-template-title]').text(template.replace(/.+\/(.+)/, '$1').replace(/[_-]/g, ' ').replace(/^(.)|\s+(.)/g, function ($1) {
+        $('[data-template-title]').text(template.replace(/.+\/(.+).html/, '$1').replace(/[_-]/g, ' ').replace(/^(.)|\s+(.)/g, function ($1) {
           return $1.toUpperCase()
         }));
-        $('[data-template-target]').html(data);
+        $('[data-template-container]').html(data);
 
-        $('pre code').each(function(i, block) {
+        $('pre code').each(function (i, block) {
           hljs.highlightBlock(block);
         });
+      })
+    }
+  };
+
+  var HAnchor = {
+    init: function () {
+      $('body').on('click', '[data-hanchor]', function () {
+        var anchor = $(this).data('hanchor');
+
+        $('[data-anchor="' + anchor + '"]').scrollTo();
       })
     }
   };
@@ -56,7 +77,18 @@
       M.AutoInit();
       Template.init();
       Sidenav.init();
+      HAnchor.init();
+
+      if (location.hash.substring(0, 2) === '#!') {
+        Template.show(location.hash.substring(2));
+      }
     }
+  };
+
+  $.fn.scrollTo = function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 300);
   };
 
   $(document).ready(Main.init);
