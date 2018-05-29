4b2b360a64c0d5e5309d8881787c99e1d9e549de
Guillaume ALLEGRET <guillaume.allegret-prestataire@laposte.fr>
2018-05-29 16:06:49 +0200

    anchor allow use href. support ie

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@
       }
     },
     show: function (template) {
-      if(Template.current === template){
+      if (Template.current === template) {
         return;
       }
 
@@ -62,7 +62,7 @@
       Loader.ajax({
         url: 'templates/' + template
       }).done(function (data) {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
 
         $('[data-template-title]').text(template.replace(/.+\/(.+).html/, '$1').replace(/[_-]/g, ' ').replace(/^(.)|\s+(.)/g, function ($1) {
           return $1.toUpperCase()
@@ -79,87 +79,81 @@
 
   var HAnchor = {
     init: function () {
-      $('body').on('click', '[data-hanchor]', function () {
-        var anchor = $(this).data('hanchor');
+      Template.onShow(HAnchor.scrollTo);
+      $(window).on("popstate", HAnchor.scrollTo);
+    },
+    scrollTo: function () {
+      var match = location.hash.match(/\#\!.+@(.+)/);
 
-        location.hash = location.hash.replace(/@.+/, '') + '@' + anchor;
-
-        $('[data-anchor="' + anchor + '"]').scrollTo();
-      });
-
-      Template.onShow(function () {
-        var match = location.hash.match(/\#\!.+@(.+)/);
-
-        if (match) {
-          $('[data-anchor="' + match[1] + '"]').scrollTo();
-        }
-      })
+      if (match) {
+        $('[data-anchor="' + match[1] + '"]').scrollTo();
+      }
     }
   };
 
-    var Summary = {
-      init: function () {
-        Template.onShow(Summary.generate)
-      },
-      generate: function () {
-        if (!$('.summary.generate').length) {
-          return;
-        }
-
-        var o = [], h2, h3;
-
-        $('h2, h3, h4').each(function () {
-          switch (this.tagName) {
-            case 'H2':
-              o.push(h2 = {
-                name: this.innerHTML,
-                slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
-                items: []
-              });
-              h3 = null;
-              break;
-            case 'H3':
-              h2.items.push(h3 = {
-                name: this.innerHTML,
-                slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
-                items: []
-              });
-              break;
-            case 'H4':
-              (h3 || h2).items.push({
-                name: this.innerHTML,
-                slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
-                items: []
-              });
-              break;
-          }
-
-          $(this).attr('data-anchor', this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'))
-        });
-
-        function _gen_li(o) {
-          var li = '<li><a data-hanchor="'+ o.slug + '">' + o.name + '</a>';
-
-          if(o.items.length){
-            li += '<ul>';
-            for(var i = 0, l = o.items.length; i < l; i++){
-              li += _gen_li(o.items[i]);
-            }
-            li += '</ul>';
-          }
-
-          return li + '</li>'
-        }
-
-        var lis = '';
-
-        for(var i = 0, l = o.length; i < l; i++){
-          lis += _gen_li(o[i]);
-        }
-
-        $('.summary.generate').html(lis);
+  var Summary = {
+    init: function () {
+      Template.onShow(Summary.generate)
+    },
+    generate: function () {
+      if (!$('.summary.generate').length) {
+        return;
       }
-    };
+
+      var o = [], h2, h3, base_url = '/' + location.hash.match(/^(\#\![^@]+)/)[1] + '@';
+
+      $('h2, h3, h4').each(function () {
+        switch (this.tagName) {
+          case 'H2':
+            o.push(h2 = {
+              name: this.innerHTML,
+              slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
+              items: []
+            });
+            h3 = null;
+            break;
+          case 'H3':
+            h2.items.push(h3 = {
+              name: this.innerHTML,
+              slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
+              items: []
+            });
+            break;
+          case 'H4':
+            (h3 || h2).items.push({
+              name: this.innerHTML,
+              slug: this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'),
+              items: []
+            });
+            break;
+        }
+
+        $(this).attr('data-anchor', this.innerText.toLowerCase().replace(/[^a-zA-Z\d]/g, '-'))
+      });
+
+      function _gen_li(o) {
+        var li = '<li><a href="' + base_url + o.slug + '">' + o.name + '</a>';
+
+        if (o.items.length) {
+          li += '<ul>';
+          for (var i = 0, l = o.items.length; i < l; i++) {
+            li += _gen_li(o.items[i]);
+          }
+          li += '</ul>';
+        }
+
+        return li + '</li>'
+      }
+
+      var lis = '';
+
+      for (var i = 0, l = o.length; i < l; i++) {
+        lis += _gen_li(o[i]);
+      }
+
+      $('.summary.generate').html(lis);
+    }
+  };
 
   var Main = {
     init: function () {
@@ -171,20 +165,36 @@
       Template.init();
 
       Template.onShow(function () {
-          Prism.highlightAll()
+        Prism.highlightAll()
       });
     },
-    ready:function () {
+    ready: function () {
       Template.ready();
     }
   };
 
   $.fn.scrollTo = function () {
-    $('html, body').animate({
-      scrollTop: $(this).offset().top
-    }, 300);
+    if ($(this).length) {
+      $('html, body').animate({
+        scrollTop: $(this).offset().top
+      }, 300);
+    }
   };
-
   $(document).ready(Main.init);
   $(window).on('load', Main.ready);
+
+  if (window.navigator.userAgent.indexOf("MSIE ")) {
+    (function () {
+      var hash = location.hash;
+      $('body').on('click', function () {
+        setTimeout(function () {
+          if (hash !== location.hash) {
+            hash = location.hash;
+            $(window).trigger('popstate');
+          }
+        })
+      })
+    })()
+  }
+
 })(window, document, jQuery, M);
